ad374042c75aaa94063c2d69f8b5e4fb1f07be1d
Zain Shah <zan2434@gmail.com>
2014-11-21 20:08:02 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.get('/dsr', 
+  function(req, res){
+    res.render('dsr', { title: 'Express' });
+  }
+);
+app.get('/paint', 
+  function(req, res){
+    res.render('paint', { title: 'Express' });
+  }
+);
+app.get('/cat', 
+  function(req, res){
+    res.render('cat', { title: 'Express' });
+  }
+);
+app.get('/sound',
+  function(req, res){
+    res.render('sound', { title: "Liquid sound recognition"})
+  }
+);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
